7ba5d8dd03ff810324e4dc3f6dbfb925516f8245
jackchou <jackzhou4job@163.com>
2021-05-16 22:49:04 +0800

    chore: 测试git 提交信息检查

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@
  * @Description: 项目入口
  * @Date: 2021-05-16 20:00:28 +0800
  * @Author: JackChou
- * @LastEditTime: 2021-05-16 21:43:17 +0800
+ * @LastEditTime: 2021-05-16 22:25:27 +0800
  * @LastEditors: JackChou
  */
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+console.log(22)
 new Vue({
   name: 'Root',
   render: h => h(App),
