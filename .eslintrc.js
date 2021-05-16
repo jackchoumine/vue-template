8ebb5ef8b9e3c9fbd44dddce196eb0f1b7be62b3
jackchou <jackzhou4job@163.com>
2021-05-16 20:34:45 +0800

    chore: 配置lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,106 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ['plugin:vue/essential', '@vue/standard'],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    quotes: [2, 'single', { allowTemplateLiterals: true }],
+    // 末尾逗号在数组和对象换行时才添加
+    'comma-dangle': [2, 'always-multiline'],
+    semi: [2, 'never'],
+    'no-new': 2, // 禁用new创建对象
+    // 强制在关键字前后使用一致的空格 (前后腰需要)
+    'keyword-spacing': 2,
+    // 强制一行的最大长度
+    'max-len': [1, 120],
+    // 使用 === 替代 == allow-null允许null和undefined==
+    eqeqeq: [2, 'allow-null'],
+    // 禁止将变量初始化为 undefined
+    'no-undef-init': 2,
+    // 禁止将 undefined 作为标识符
+    // NOTE 使用 void 0 代替 undefined
+    'no-undefined': 2,
+    // 禁止出现未使用过的变量
+    // NOTE 不要全局关闭，在文件内和当前行关闭该规则的检查: cmd + .
+    'no-unused-vars': [2, { vars: 'all', args: 'none' }],
+    // 要求使用 const 声明那些声明后不再被修改的变量
+    'no-var': 2,
+    'prefer-const': 2,
+    'spaced-comment': [2, 'always'],
+    'vue/require-name-property': 2,
+    'vue/multiline-html-element-content-newline': 0,
+    // 一行多少属性
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        singleline: 4,
+        multiline: {
+          max: 4,
+          allowFirstLine: true,
+        },
+      },
+    ],
+    'vue/html-indent': [
+      2,
+      2,
+      {
+        attribute: 1, // 属性缩进
+        baseIndent: 1,
+        closeBracket: 0,
+        alignAttributesVertically: true,
+        ignores: [],
+      },
+    ],
+    // https://eslint.vuejs.org/rules/attributes-order.html
+    'vue/attributes-order': [
+      2,
+      {
+        order: [
+          'DEFINITION',
+          'LIST_RENDERING',
+          'CONDITIONALS',
+          'RENDER_MODIFIERS',
+          'GLOBAL',
+          'UNIQUE',
+          'TWO_WAY_BINDING',
+          'OTHER_DIRECTIVES',
+          'OTHER_ATTR',
+          'EVENTS',
+          'CONTENT',
+        ],
+        alphabetical: false,
+      },
+    ],
+    'vue/order-in-components': [
+      2,
+      {
+        order: [
+          'el',
+          'name',
+          'parent',
+          'functional',
+          ['delimiters', 'comments'],
+          ['components', 'directives', 'filters'],
+          'extends',
+          'mixins',
+          'inheritAttrs',
+          'model',
+          ['props', 'propsData'],
+          'data',
+          'computed',
+          'watch',
+          'LIFECYCLE_HOOKS',
+          'methods',
+          ['template', 'render'],
+          'renderError',
+        ],
+      },
+    ],
+  },
 }
