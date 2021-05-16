2adff069ad8fa2dd374b8ebfe88ca163bb876c47
jackchou <jackzhou4job@163.com>
2021-05-16 21:02:10 +0800

    测试husky

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Description: 项目入口
  * @Date: 2021-05-16 20:00:28 +0800
  * @Author: JackChou
- * @LastEditTime: 2021-05-16 20:51:49 +0800
+ * @LastEditTime: 2021-05-16 21:01:55 +0800
  * @LastEditors: JackChou
  */
 import Vue from 'vue'
@@ -10,8 +10,7 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-console.log(24)
-// eslint-disable-next-line vue/require-name-property
 new Vue({
+  name: 'Rooter',
   render: h => h(App),
 }).$mount('#app')
