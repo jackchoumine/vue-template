341d43d7f3880e7840ca3aba4b7171da60d21d9d
jackchou <jackzhou4job@163.com>
2021-05-16 20:52:23 +0800

    chore: 替换husky版本

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Description: 项目入口
  * @Date: 2021-05-16 20:00:28 +0800
  * @Author: JackChou
- * @LastEditTime: 2021-05-16 20:49:22 +0800
+ * @LastEditTime: 2021-05-16 20:51:49 +0800
  * @LastEditors: JackChou
  */
 import Vue from 'vue'
@@ -10,7 +10,7 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-console.log(22)
+console.log(24)
 // eslint-disable-next-line vue/require-name-property
 new Vue({
   render: h => h(App),
