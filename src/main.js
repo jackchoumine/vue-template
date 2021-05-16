9d67d39e1ab0ffa2bda85166059285b8a887ac2d
jackchou <jackzhou4job@163.com>
2021-05-16 21:43:49 +0800

    chore: 修改组件名称

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Description: 项目入口
  * @Date: 2021-05-16 20:00:28 +0800
  * @Author: JackChou
- * @LastEditTime: 2021-05-16 21:01:55 +0800
+ * @LastEditTime: 2021-05-16 21:43:17 +0800
  * @LastEditors: JackChou
  */
 import Vue from 'vue'
@@ -11,6 +11,6 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
-  name: 'Rooter',
+  name: 'Root',
   render: h => h(App),
 }).$mount('#app')
